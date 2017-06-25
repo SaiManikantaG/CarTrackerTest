f2db486b73f07010bc5bcf963d32c1ef277b9ed2
SaiManikantaG <infomanikanta@gmail.com>
2017-06-25 18:42:10 -0500

    New changes with complete REST implementation

---
+++ src/main/java/com/tracker/com/tracker/entity/Readings.java
@@ -0,0 +1,131 @@
+package com.tracker.com.tracker.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by sai on 6/25/17.
+ */
+
+@Entity
+public class Readings {
+
+    @Id
+    private String vin;
+
+    private double latitude;
+    private double longitute;
+    private String timestamp;
+    private double fuelVolume;
+    private double speed;
+    private int engineHp;
+    private boolean checkEngineLightOn;
+    private boolean engineCoolantLow;
+    private boolean cruiseControlOn;
+    private int engineRpm;
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitute() {
+        return longitute;
+    }
+
+    public void setLongitute(double longitute) {
+        this.longitute = longitute;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getFuelVolume() {
+        return fuelVolume;
+    }
+
+    public void setFuelVolume(double fuelVolume) {
+        this.fuelVolume = fuelVolume;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public int getEngineHp() {
+        return engineHp;
+    }
+
+    public void setEngineHp(int engineHp) {
+        this.engineHp = engineHp;
+    }
+
+    public boolean isCheckEngineLightOn() {
+        return checkEngineLightOn;
+    }
+
+    public void setCheckEngineLightOn(boolean checkEngineLightOn) {
+        this.checkEngineLightOn = checkEngineLightOn;
+    }
+
+    public boolean isEngineCoolantLow() {
+        return engineCoolantLow;
+    }
+
+    public void setEngineCoolantLow(boolean engineCoolantLow) {
+        this.engineCoolantLow = engineCoolantLow;
+    }
+
+    public boolean isCruiseControlOn() {
+        return cruiseControlOn;
+    }
+
+    public void setCruiseControlOn(boolean cruiseControlOn) {
+        this.cruiseControlOn = cruiseControlOn;
+    }
+
+    public int getEngineRpm() {
+        return engineRpm;
+    }
+
+    public void setEngineRpm(int engineRpm) {
+        this.engineRpm = engineRpm;
+    }
+
+    @Override
+    public String toString() {
+        return "Readings{" +
+                "vin='" + vin + '\'' +
+                ", latitude=" + latitude +
+                ", longitute=" + longitute +
+                ", timestamp='" + timestamp + '\'' +
+                ", fuelVolume=" + fuelVolume +
+                ", speed=" + speed +
+                ", engineHp=" + engineHp +
+                ", checkEngineLightOn=" + checkEngineLightOn +
+                ", engineCoolantLow=" + engineCoolantLow +
+                ", cruiseControlOn=" + cruiseControlOn +
+                ", engineRpm=" + engineRpm +
+                '}';
+    }
+}
